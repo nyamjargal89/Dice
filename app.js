9b935f90c3f4be7e38bac783cab4c9c47002c50e
Nyamaa <tu.nyamjargal@gmail.com>
2020-03-07 12:49:14 +0800

    Shoog shiddeg bolgoh

--- app.js
+++ app.js
@@ -5,20 +5,25 @@ var activePlayer = 1;
 var scores = [0, 0];
 
 // Тоглогчийн ээлжиндээ цуглуулж байгаа оноог хадгалах хувьсагч
-var roundScore = 0 ;
+var roundScore = 0;
 
 // Шооны аль талаараа буусныг хадгалах хувьсагч хэрэгтэй, 1-6 гэсэн утгыг энэ хувьсагчид санамсаргүйгээр үүсгэж өгнө.
 
-var diceNumber =Math.floor( Math.random()*6)+1;
+var diceNumber = Math.floor(Math.random() * 6) + 1;
 // <div class="player-score" id="score-0">43</div>
 // window.document.querySelector('#score-0').textContent = dice;
 // document.querySelector('#score-1').innerHTML = "<em>YES!</em>";
 // Програм эхлэхэд бэлтгэе.
-document.querySelector('.dice').style.display="none";
-window.document.querySelector('#score-0').textContent = 0;
-window.document.querySelector('#score-1').textContent = 0;
 
-document.querySelector('#current-0').textContent = 0;
-document.querySelector('#current-1').textContent = 0;
-
-console.log("Шоо : " + dice);
+document.getElementById("score-0").textContent = "0";
+document.getElementById("score-1").textContent = "0";
+document.getElementById("current-0").textContent = "0";
+document.getElementById("current-1").textContent = "0";
+var diceDom = document.querySelector(".dice")
+diceDom.style.display = "none";
+document.querySelector(".btn-roll").addEventListener("click", function() {
+  var diceNumber = Math.floor(Math.random() * 6) + 1;
+  diceDom.style.display = "block";
+  diceDom.src = 'dice-'+ diceNumber + ".png"
+  
+});
