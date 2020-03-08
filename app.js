2aeedbd0977c46bd6496c6ef57875816ae3a811d
Nyamaa <tu.nyamjargal@gmail.com>
2020-03-08 17:04:13 +0800

    Toglogchiin eeljiig solidog bolgono

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 // Тоглогчийн ээлжийг хадгалах хувьсагч, нэгдүгээр тоглогчийг 0, хоёрдугаар тоглогчийг 1 гэж тэмдэглэнэ.
-var activePlayer = 1;
+var activePlayer = 0;
 
 // Тоглогчдын цуглуулсан оноог хадгалах хувьсагч
 var scores = [0, 0];
@@ -19,11 +19,39 @@ document.getElementById("score-0").textContent = "0";
 document.getElementById("score-1").textContent = "0";
 document.getElementById("current-0").textContent = "0";
 document.getElementById("current-1").textContent = "0";
-var diceDom = document.querySelector(".dice")
+var diceDom = document.querySelector(".dice");
 diceDom.style.display = "none";
+// Шоог шидэх эвент листенер
 document.querySelector(".btn-roll").addEventListener("click", function() {
+  // 1 - 6 доторх санамсаргүй нэг тоо гаргаж авна
   var diceNumber = Math.floor(Math.random() * 6) + 1;
+  // Шооны зургийг вэб дээр гаргаж ирнэ.
   diceDom.style.display = "block";
-  diceDom.src = 'dice-'+ diceNumber + ".png"
-  
+  //   Буусан санамсаргүй тоонд харгарзах шооны зургийг вэб дээр гаргаж ирнэ.
+  diceDom.src = "dice-" + diceNumber + ".png";
+  //   Буусан тоо нь 1 ээс ялгаатай бол идэвхтэй тоглогчийн ээлжийн оноог нэмэгдүүлнэ.
+  if (diceNumber !== 1) {
+    // ялгаа тоо буулаа. Буусан тоог тоглогчид нэмж өгнө
+    roundScore = roundScore + diceNumber;
+    document.getElementById("current-" + activePlayer).textContent = roundScore;
+  } else {
+    // 1 буусан тул тоглогчийн ээлжийг энэ хэсэгт сольж өгнө.
+    //  Энэ тоглогчийн ээлжиндээ цуглуулсан оноог 0 болгоно.
+    roundScore = 0;
+    document.getElementById("current-" + activePlayer).textContent = 0;
+    // хэрэв идэвхтэй тоглогч нь 0 байвал идэвхтэй тоглогчийг 1 болго.
+    // Үгүй бол идэвхтэй тоглогчийг 0 болго.
+    // Тоглогчийн ээлжийг нөгөө тоглогч руу шилжүүлнэ.
+    // activePlayer === 0 ? (activePlayer = 1) : (activePlayer = 0);
+    if (activePlayer === 0) {
+      activePlayer = 1;
+    } else {
+      activePlayer = 0;
+    }
+    // Улаан цэгийг хайж олно.
+    document.querySelector(".player-0-panel").classList.toggle("active");
+    document.querySelector(".player-1-panel").classList.toggle("active");
+    // Шоог түр алга болгоно.
+    diceDom.style.display = "none";
+  }
 });
